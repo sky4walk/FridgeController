99b268835781adc11997b74b18d1bdb8a103ddff
Andre Betz <git@andrebetz.de>
2021-06-10 09:47:03 +0200

    added test for RC Switch

---
+++ sw/src/SendDemo/SendDemo.ino
@@ -0,0 +1,29 @@
+/*
+  Example for different sending methods
+  
+  https://github.com/sui77/rc-switch/
+  
+*/
+
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+
+  Serial.begin(115200);
+  
+  // WiiMo D1 moni D3  
+  mySwitch.enableTransmit(0);
+  mySwitch.setProtocol(1);
+  mySwitch.setPulseLength(315);  
+  //mySwitch.setRepeatTransmit(15);  
+}
+
+void loop() {
+  /* Same switch as above, but using decimal code */
+  mySwitch.send(1631343, 24);
+  delay(2500);  
+  mySwitch.send(1631342, 24);
+  delay(2500);   
+}
